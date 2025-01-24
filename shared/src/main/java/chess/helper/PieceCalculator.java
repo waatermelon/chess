b78d86e900e957b95530c886cb865055442166dc
c4zombieslayer <c4zombieslayer@gmail.com>
2025-01-23 23:51:43 -0700

    commenting

--- shared/src/main/java/chess/helper/PieceCalculator.java
+++ shared/src/main/java/chess/helper/PieceCalculator.java
@@ -18,35 +18,72 @@ public class PieceCalculator {
         this.teamColor = teamColor;
     }
 
+
+    /***
+     *
+     * @param position  The current position of the piece.
+     * @param row       The row(y/i) movement. (-1, 0, 1)
+     * @param col       The column(x/j) movement. (-1, 0, 1)
+     * @return          The possible moves based off of the given row and column directions.
+     */
+    private Collection<ChessMove> directionalMove(ChessPosition position, int row, int col) {
+        ArrayList<int[]> moves = new ArrayList<>();
+
+        for(int i = position.getRow() + row; row != 0 && i < ((row > 0) ? 8 : -1); i += row)
+            moves.add(new int[]{0, i});
+
+        for(int j = position.getColumn() + col, k = 0, l = 0; col != 0 && j < ((col > 0) ? 8 : -1); j += col) {
+            ++l;
+            if(moves.isEmpty()) k = 1;
+            if(k == 1)
+                moves.add(new int[]{j, 0});
+            else
+                moves.get(l)[0] = j;
+        }
+
+        ArrayList<ChessMove> movesList = new ArrayList<>();
+        for (int[] move : moves)
+            movesList.add(new ChessMove(position, new ChessPosition(move[1], move[0]), type));
+
+        return movesList;
+    }
+
     private Collection<ChessMove> pawnMoves(ChessPosition position) {
         ArrayList<ChessMove> moves = new ArrayList<>();
         int yChange = (teamColor == ChessGame.TeamColor.WHITE) ? 1 : -1;
         moves.add(new ChessMove(position, new ChessPosition(position.getRow() + yChange, position.getColumn()), type));
         return moves;
     }
-    private Collection<ChessMove> knightMoves(ChessPosition position) {
-        return new ArrayList<>();//TODO
-    }
-    private Collection<ChessMove> rookMoves(ChessPosition position) {
-        return new ArrayList<>();//TODO
 
+    private Collection<ChessMove> knightMoves(ChessPosition position) {
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        for(int i = 0; i < 4; ++i) {
+            for(int j = -1; j < 1; ++j) {
+
+            }
+        }
+
+        return moves;
     }
+
+    private Collection<ChessMove> rookMoves(ChessPosition position) {
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        moves.addAll(directionalMove(position, 0, -1));
+        moves.addAll(directionalMove(position, 0, 1));
+        moves.addAll(directionalMove(position, -1, 0));
+        moves.addAll(directionalMove(position, -1, 0));
+
+        return moves;
+    }
+
     private Collection<ChessMove> bishopMoves(ChessPosition position) {
         ArrayList<ChessMove> moves = new ArrayList<>();
-        /*
-        * - bishop travels on diagonals
-        * - from position
-        * calculate amount of squares on each side (left, right),
-        *  - right = (7 - position.column)
-        *  - left =  (7 - right)
-        *
-        * calculate square on top and bottom (up, down)
-        *  - up = (7 - position.row)
-        *  - down = (7 - up)
-        * loop through
-        *
-        * for each
-        * */
+        moves.addAll(directionalMove(position, 1, -1));
+        moves.addAll(directionalMove(position, 1, 1));
+        moves.addAll(directionalMove(position, -1, -1));
+        moves.addAll(directionalMove(position, -1, 1));
+
+        return moves;
     }
     private Collection<ChessMove> kingMoves(ChessPosition position) {
         ArrayList<ChessMove> moves = new ArrayList<>();
@@ -59,7 +96,11 @@ public class PieceCalculator {
         return moves;
     }
     private Collection<ChessMove> queenMoves(ChessPosition position) {
-        return new ArrayList<>();//TODO
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        moves.addAll(bishopMoves(position));
+        moves.addAll(rookMoves(position));
+
+        return moves;
     }
 
     public static Collection<ChessMove> possibleMoves(ChessPiece.PieceType type, ChessGame.TeamColor teamColor, ChessPosition position) {
