2dd37c34fbb770771cfabd5cfa7e7c5f8470d69b
c4zombieslayer <c4zombieslayer@gmail.com>
2025-02-26 23:14:49 -0700

    implemented MemoryAuthDAO class

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,57 @@
 package dataaccess;
 
-public class MemoryAuthDAO {
+import model.AuthData;
+
+import java.util.ArrayList;
+
+public class MemoryAuthDAO implements AuthDAO{
+
+    ArrayList<AuthData> db;
+
+    public MemoryAuthDAO(ArrayList<AuthData> initAuthData) {
+        db = initAuthData;
+    }
+
+    public MemoryAuthDAO() {
+        db = new ArrayList<AuthData>();
+    }
+
+    // Create
+    @Override
+    public void createAuth(AuthData authData) {
+        for (AuthData dbAuthData: db) {
+            if (dbAuthData.equals(authData)) {
+                return;
+            }
+        }
+        db.add(authData);
+    }
+
+    // Read
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        for (AuthData dbAuthData: db) {
+            if (dbAuthData.authToken().equals(authToken)) {
+                return dbAuthData;
+            }
+        }
+        throw new DataAccessException("Token was not found.");
+    }
+
+    // Update/Delete
+    @Override
+    public void deleteAuth(AuthData authData) {
+        for (int i = 0; i < db.size(); ++i) {
+            if (db.get(i).equals(authData)) {
+                db.remove(i);
+                return;
+            }
+        }
+    }
+
+    // Delete
+    @Override
+    public void clear() {
+        db.clear();
+    }
 }
