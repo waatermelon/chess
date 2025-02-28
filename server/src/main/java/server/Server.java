c673caf7acf20cdf9600b5a68c238954fdcdf7c7
c4zombieslayer <c4zombieslayer@gmail.com>
2025-02-27 23:48:22 -0700

    commented files todo

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,7 +10,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
+        //TODO
         //This line initializes the server and can be removed once you have a functioning endpoint
         Spark.init();
 
