1bd1589f9d45ba23a36a0d49ddc9a6e91b61e65e
c4zombieslayer <c4zombieslayer@gmail.com>
2025-03-03 03:02:31 -0700

    Added Server Endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -26,9 +26,21 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-        //TODO
+
+        Spark.delete("/db", this::clear);
+        Spark.post("/user", userHandler::register);
+        Spark.post("/session", authHandler::login);
+        Spark.delete("/session", authHandler::logout);
+        Spark.get("/game", gameHandler::listGames);
+        Spark.post("/game", gameHandler::createGame);
+        Spark.put("/game", gameHandler::joinGame);
+
+        // TODO work on creating exceptions next
+        // TODO work on service tests, and others as well if time permits
+        //Spark.exception();
+
         //This line initializes the server and can be removed once you have a functioning endpoint
-        Spark.init();
+        //Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -37,5 +49,17 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private Object clear(Request request, Response response) {
+        authHandler.clear();
+        gameHandler.clear();
+        userHandler.clear();
+        response.status(200);
+        return "{}";
+    }
+
+    private void unauthorized() {
+
     }
 }
