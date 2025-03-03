2bbb2c53f5a8965968d3bae0221c9acf0d96bfbf
c4zombieslayer <c4zombieslayer@gmail.com>
2025-03-03 02:50:24 -0700

    finished GameHandler

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -1,9 +1,14 @@
 package server;
 
 import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import model.GameData;
+import model.JoinRequestData;
 import service.GameService;
 import spark.Request;
 import spark.Response;
+
+import java.util.ArrayList;
 
 public class GameHandler {
 
@@ -14,24 +19,26 @@ public class GameHandler {
         this.gameService = gameService;
     }
 
-    public Object listGames(Request request, Response response) {
-        //implement with service//TODO
-        return null;
+    public Object listGames(Request request, Response response) throws DataAccessException {
+        String authToken = request.headers("authorization");
+        ArrayList<GameData> gamesList = gameService.listGames(authToken);
+        response.status(200);
+        return "{games: " + gamesList + "}";
     }
 
-    public Object createGame(Request request, Response response) {
-        //implement with service//TODO
-        return null;
+    public Object createGame(Request request, Response response) throws DataAccessException {
+        String authToken = request.headers("authorization");
+        int gameID = gameService.createGame(authToken);
+        response.status(200);
+        return "{gameID: " + gameID + "}";
     }
 
-    public Object joinGame(Request request, Response response) {
-        //implement with service//TODO
-        return null;
-    }
-
-    public Object clearApplication(Request request, Response response) {
-        //implement with service//TODO
-        return null;
+    public Object joinGame(Request request, Response response) throws DataAccessException {
+        String authToken = request.headers("authorization");
+        JoinRequestData jrq = serializer.fromJson(request.body(), JoinRequestData.class);
+        gameService.joinGame(authToken, jrq.playerColor(), jrq.gameID());
+        response.status(200);
+        return "{}";
     }
 
     public void clear() {
---
+++ shared/src/main/java/model/JoinRequestData.java
@@ -0,0 +1,6 @@
+package model;
+
+import chess.ChessGame;
+
+public record JoinRequestData(int gameID, String playerColor) {
+}
