09142c0426e6542a9e265fe3dd60a7fbffc778c0
c4zombieslayer <c4zombieslayer@gmail.com>
2025-03-25 05:47:56 -0600

    updated sqldao

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -91,7 +91,7 @@ public class SQLGameDAO implements GameDAO {
             preparedStatement.setString(4, serializer.toJson(gameData.game()));
             preparedStatement.setInt(5, gameData.gameID());
             // setup for next part
-            // testing
+            // testin
             int rowsUpdated = preparedStatement.executeUpdate();
             if (rowsUpdated == 0) {
                 throw new DataAccessException("Item requested to be updated not found");
