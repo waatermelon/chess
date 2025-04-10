786ab85dd4690cbdde8f1880363ce001dd032617
c4zombieslayer <c4zombieslayer@gmail.com>
2025-04-09 21:35:50 -0600

    imported user game command class

---
+++ shared/src/main/java/model/websocket/UserGameCommand.java
@@ -0,0 +1,14 @@
+package model.websocket;
+
+public class UserGameCommand {
+    public enum CommandType {
+        CONNECT,
+        MAKE_MOVE,
+        LEAVE,
+        RESIGN
+    }
+
+    public CommandType commandType;
+    public String authToken;
+    public Integer gameID;
+}
