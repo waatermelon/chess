c9485d70a86dfb0550b971730347f32ae1a2fa95
c4zombieslayer <c4zombieslayer@gmail.com>
2025-04-11 16:42:49 -0600

    setup for autograder

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
 
-        ChessLoop chessLoop = new ChessLoop(8080, false);
+        ChessLoop chessLoop = new ChessLoop(0, false);
         chessLoop.run();
     }
 }
